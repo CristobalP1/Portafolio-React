75bae11beb2394cba861cee5d6965d33663671f2
cristobal-1 <cristobal1@gmail.com>
2022-12-13 04:33:22 -0300

    fix navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavbarPage } from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CollapsibleExample from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <NavbarPage></NavbarPage>
+{/*       <CollapsibleExample></CollapsibleExample> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import logo from '../assets/svg/logo.svg'
 import navGmail from '../assets/svg/navGmail.svg'
 import navInstagram from '../assets/svg/navInstagram.svg'
 import navTiktok from '../assets/svg/navTiktok.svg'
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 export const NavbarPage = () => {
@@ -30,7 +31,7 @@ export const NavbarPage = () => {
     }
 
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+/*         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
           <Container>
             <Navbar.Brand href="#home"><img src={logo} alt='Logo'>
             </img>
@@ -55,6 +56,31 @@ export const NavbarPage = () => {
               </span>
               </Nav.Collapse>
           </Container>
-        </Navbar>
+        </Navbar> */
+
+      <Navbar expand="lg" bg="dark" variant="dark" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} alt='Logo'></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"><span className='navbar-toggler-icon'></span></Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activateLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activateLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activateLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className='navbar-text'>
+                <div className='social-icon'>
+                    <a href="#"><img src={navInstagram} alt="Instagram Logo"></img></a>
+                    <a href="#"><img src={navTiktok} alt="Tiktok Logo"></img></a>
+                    <a href="#"><img src={navGmail} alt="Gmail Logo"></img></a>
+                </div>
+                <button className='vvd' onClick={()=>{console.log('Holaa')}}><span>Let's Connect</span></button>
+              </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
       )
 }
